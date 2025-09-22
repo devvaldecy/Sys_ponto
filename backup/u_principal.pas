251d2665831e361df3e2510bd09ffbbdf3e8475a
devvaldecy <valdecytecnico@live.com>
2025-09-22 10:27:31 -0300

    Alterações na tela inicial

--- backup/u_principal.pas
+++ backup/u_principal.pas
@@ -5,10 +5,20 @@ unit u_principal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls,
+  StdCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TFrmPrincipal }
+
+  TFrmPrincipal = class(TForm)
+    Label1: TLabel;
+    Panel1: TPanel;
+    PnlTop: TPanel;
+    Sb: TStatusBar;
+    Sp01: TShape;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -16,11 +26,18 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FrmPrincipal: TFrmPrincipal;
 
 implementation
 
 {$R *.lfm}
+
+{ TFrmPrincipal }
+
+procedure TFrmPrincipal.FormCreate(Sender: TObject);
+begin
+
+end;
 
 end.
 
--- u_principal.pas
+++ u_principal.pas
@@ -5,10 +5,21 @@ unit u_principal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls,
+  StdCtrls;
 
 type
+
+  { TFrmPrincipal }
+
   TFrmPrincipal = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Panel1: TPanel;
+    PnlTop: TPanel;
+    Sb: TStatusBar;
+    Sp01: TShape;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -21,6 +32,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFrmPrincipal }
+
+procedure TFrmPrincipal.FormCreate(Sender: TObject);
+begin
+
+end;
 
 end.
 
